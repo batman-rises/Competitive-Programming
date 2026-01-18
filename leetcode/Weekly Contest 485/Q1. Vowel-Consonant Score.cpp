4304255f3df0fc707d8af752e2bdd15391d2164c
Binayak Panda <pbinayak604@gmail.com>
2026-01-18 18:34:52 +0530

    Jan 18,2025

---
+++ leetcode/Weekly Contest 485/Q1. Vowel-Consonant Score.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int vowelConsonantScore(string s) {
+        int v=0,c=0;
+        for(char ch:s){
+            if(ch>='a' && ch<='z'){
+                if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                    v++;
+                }else{
+                    c++;
+                }
+            }
+        }
+        if(c>0)return floor(v/c);
+        else
+            return 0;
+    }
+};
